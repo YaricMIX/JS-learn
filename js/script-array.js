dac269c15c2527badb81763cde046a267f86b92e
YaricMIX <aroslavlivachov@gmail.com>
2023-03-09 21:49:38 +0100

    made functions

--- js/script-array.js
+++ js/script-array.js
@@ -1,33 +1,98 @@
-const array = [2, 7, 2, 0, 1, 2, 4, 'a', 7, 0];
-let sum = 0;
-let min = array[0];
-let max = array[0];
-
-for (let i = 0; i < array.length; i++) {
-	if (typeof array[i] === 'number') {
-		sum += array[i];
+function add() {
+	const a = prompt("Enter the first number:");
+	if (isNaN(a)) {
+		console.log("The first parameter is not a number!");
+		return NaN;
 	}
+	const b = prompt("Enter the second number:");
+	if (isNaN(b)) {
+		console.log("The second parameter is not a number!");
+		return NaN;
+	}
+	const result = Number(a) + Number(b);
+	console.log(`Addition result ${a} and ${b}: ${result}`);
+	return result;
+}
+
+function subtract() {
+	const a = prompt("Enter the first number:");
+	if (isNaN(a)) {
+		console.log("The first parameter is not a number!");
+		return NaN;
+	}
+	const b = prompt("Enter the second number:");
+	if (isNaN(b)) {
+		console.log("The second parameter is not a number!");
+		return NaN;
+	}
+	const result = Number(a) - Number(b);
+	console.log(`Subtraction result ${b} from ${a}: ${result}`);
+	return result;
+}
+
+function multiply() {
+	const a = prompt("Enter the first number:");
+	if (isNaN(a)) {
+		console.log("The first parameter is not a number!");
+		return NaN;
+	}
+	const b = prompt("Enter the second number:");
+	if (isNaN(b)) {
+		console.log("The second parameter is not a number!");
+		return NaN;
+	}
+	const result = Number(a) * Number(b);
+	console.log(`The result of multiplication ${a} on ${b}: ${result}`);
+	return result;
+}
+
+function divide() {
+	const a = prompt("Enter the first number:");
+	if (isNaN(a)) {
+		console.log("The first parameter is not a number!");
+		return NaN;
+	}
+	const b = prompt("Enter the second number:");
+	if (isNaN(b)) {
+		console.log("The second parameter is not a number!");
+		return NaN;
+	}
+	if (Number(b) === 0) {
+		console.log("The second parameter cannot be zero!");
+		return NaN;
+	}
+	const result = Number(a) / Number(b);
+	console.log(`The result of division ${a} on ${b}: ${result}`);
+	return result;
 }
 
 
-for (let i = 0; i < array.length; i++) {
-	if (typeof array[i] === 'number' && !isNaN(array[i])) {
-	if (min === null || array[i] < min) {
+const array = [2, 7, 2, 250, 1, 2, 4, 'a', 7, 0];
+
+function findMin(array) {
+	if (!Array.isArray(array)) {
+		console.log('Error: argument should be an array');
+		return;
+	}
+	let min = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] < min) {
 		min = array[i];
 		}
-	if (max === null || array[i] > max) {
+	}
+	return min;
+}
+
+function findMax(array) {
+	if (!Array.isArray(array)) {
+		console.log('Error: argument should be an array');
+		return;
+	}
+	let max = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] > max) {
 		max = array[i];
 		}
 	}
+	return max;
 }
-
-let pattern = '';
-for (let i = 1; i <= 5; i++) {
-	pattern += '#'.repeat(i) + '\n';
-}
-
-console.log(array);
-console.log(sum);
-console.log('Minimum value:', min);
-console.log('Maximum value:', max);
-console.log(pattern);
